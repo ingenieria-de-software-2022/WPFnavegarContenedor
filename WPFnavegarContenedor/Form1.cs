6d5d20ba0b951544500e2e174970c6c816f250a9
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-04-22 16:30:26 -0500

    feat: adding tab navigator on WPF

---
+++ WPFnavegarContenedor/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WPFnavegarContenedor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGraphic_Click(object sender, EventArgs e)
+        {
+            double y;
+
+            for (int x = 0; x <= 100; x++)
+            {
+                y = Math.Cos(x);
+                chart1.Series["Series1"].Points.AddXY(x, y);
+            }
+        }
+    }
+}
---
+++ WPFnavegarContenedor/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFnavegarContenedor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Window1 w1 = new Window1();
+            w1.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+
+        private void buttonPage1_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(new Page1());
+        }
+
+        private void buttonPage2_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(new Page2());
+        }
+
+        private void buttonPage3_Click(object sender, RoutedEventArgs e)
+        {
+           frame1.Navigate(new Page3());
+        }
+
+        private void buttonSignature_Click(object sender, RoutedEventArgs e)
+        {
+           System.Diagnostics.Process.Start("WFcruzlara_signature.exe");
+        }
+    }
+}
